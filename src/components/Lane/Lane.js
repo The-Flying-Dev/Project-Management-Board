0fa626790f28b5f45a4cbbf03032f2a738854bac
The-Flying-Dev <leandranm@outlook.com>
2022-07-05 22:45:46 -0700

    styled components

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,10 +1,31 @@
+import styled from 'style-components';
 import Task from '../Task/Task'
-import './Lane.css';
+//import './Lane.css';
+
+const LaneWrapper = styled.div`
+  text-align: left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 20vw;
+
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+  }
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
 
 function Lane({ laneId, title, loading, error, tasks, onDragStart, onDragOver, onDrop }) {
   return (
-    <div className='Lane-wrapper'>
-      <h2>{title}</h2>
+    <LaneWrapper>
+      <Title>{title}</Title>
       {loading || error ? ( 
         <span>{error || 'loading...'}</span>
       ) : (
@@ -20,7 +41,7 @@ function Lane({ laneId, title, loading, error, tasks, onDragStart, onDragOver, o
           />
         ))      
       )}
-    </div>
+    </LaneWrapper>
   );
 }
 
